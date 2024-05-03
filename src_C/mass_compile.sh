166d007119b128f4979c37680d8d0fb4bb438742
herkinlj <127352362+herkinlj@users.noreply.github.com>
2024-05-03 15:30:42 -0400

    Create mass_compile.sh

---
+++ src_C/mass_compile.sh
@@ -0,0 +1,7 @@
+gcc pgrep_1.c -o pgrep1 -lpthread 
+gcc pgrep_2.c -o pgrep2 -lpthread 
+gcc pgrep_4.c -o pgrep4 -lpthread
+gcc pgrep_8.c -o pgrep8 -lpthread
+gcc pgrep_16.c -o pgrep16 -lpthread 
+gcc pgrep_32.c -o pgrep32 -lpthread 
+gcc pgrep_64.c -o pgrep64 -lpthread 
